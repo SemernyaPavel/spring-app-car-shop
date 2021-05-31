e77c4f6fbe2f48c8c73a70c12ff54b0979e3d232
Pavel Semernya <semernya2001@gmail.com>
2021-05-31 14:04:14 +0300

    Fix post request

--- src/main/java/by/pavel/CarShop/WebSecurityConfig.java
+++ src/main/java/by/pavel/CarShop/WebSecurityConfig.java
@@ -23,8 +23,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+		//CSRF is turn off
+			.csrf().disable()
 			.authorizeRequests()
-				.antMatchers("/home", "/registration").permitAll()
+				.antMatchers("/home", "/registration", "/css/**", "/js/**", "/images/**").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
